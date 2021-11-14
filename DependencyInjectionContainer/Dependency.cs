e72afd1dc9fd867cd31950cb6deaa75c3662c71b
Kiryl <kirillturok1@gmail.com>
2021-11-14 14:56:20 +0300

    Created enum for lifecycle and class for dependency

---
+++ DependencyInjectionContainer/Dependency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DependencyInjectionContainer
+{
+    public class Dependency
+    {
+        public Type Type { get; }
+
+        public LifeCycle LifeType { get; }
+
+        public object Key { get; }
+
+        public object Instance { get; set; }
+
+        public Dependency(Type type, LifeCycle lifeType, object key)
+        {
+            Key = key;
+            Type = type;
+            LifeType = lifeType;
+        }
+    }
+}
